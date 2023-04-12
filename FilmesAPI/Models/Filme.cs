fb1044f1cf5833a17bbe988a6cbabf1b888eb6b8
André Sampaio <andresampaio@id.uff.br>
2023-04-12 16:08:51 -0300

    Model Filme and FilmeController

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage = "The movie title is mandatory!")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "The movie genre is mandatory!")]
+    [MaxLength(50, ErrorMessage = "The movie genre mustn't have more then 50 caracters!")]
+
+    public string Genero { get; set; }
+
+    [Required(ErrorMessage = "The movie duration is mandatory!")]
+    [Range(70, 600, ErrorMessage = "The movie duration must be between 70 and 600 minutes!")]
+    public int Duracao { get; set; }
+}
