539c5896925fc061c5b4dbd071a18fcf3b34ca9b
André Sampaio <andresampaio@id.uff.br>
2023-04-19 15:15:33 -0300

    Initial Documentation

--- FilmesAPI/Program.cs
+++ FilmesAPI/Program.cs
@@ -1,5 +1,7 @@
 using FilmesApi.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 namespace FilmesAPI
 {
@@ -21,7 +23,13 @@ namespace FilmesAPI
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FilmesAPI", Version = "v1" });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
 
             var app = builder.Build();
 
