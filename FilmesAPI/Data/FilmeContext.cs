509466176b653f8dcc9cb44c9c241dce0b23bd4a
André Sampaio <andresampaio@id.uff.br>
2023-04-20 16:04:37 -0300

    API expantion adding Endereco

--- FilmesAPI/Data/FilmeContext.cs
+++ FilmesAPI/Data/FilmeContext.cs
@@ -12,6 +12,7 @@ namespace FilmesApi.Data
         }
         public DbSet<Filme> Filmes { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
     }
 
 }
