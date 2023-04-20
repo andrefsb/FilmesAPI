f6aafa28d12ca2a409abac3c27f7c5d1742fa93e
André Sampaio <andresampaio@id.uff.br>
2023-04-20 15:47:21 -0300

    API expansion adding Cinema

---
+++ FilmesAPI/Data/Dtos/CreateCinemaDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class CreateCinemaDto
+    {
+        [Required(ErrorMessage = "The field nome is mandatory.")]
+        public string Nome { get; set; }
+    }
+}
---
+++ FilmesAPI/Models/Cinema.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Cinema
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        
+        [Required (ErrorMessage = "The field nome is mandatory.")]
+        public string Nome { get; set; }
+    }
+}
